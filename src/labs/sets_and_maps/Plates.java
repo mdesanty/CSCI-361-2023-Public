16d2477f0a0d8ca0ed1a49a43766d6b07bd1ddd0
Michael DeSanty <michael.desanty@gmail.com>
2023-02-13 19:23:05 -0500

    Added Set stuff

--- src/labs/sets_and_maps/Plates.java
+++ src/labs/sets_and_maps/Plates.java
@@ -1,13 +1,18 @@
 package labs.sets_and_maps;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Plates {
     private Map<String, Integer> plateMap;
+    private Set<String> validStates;
 
     public Plates() {
         plateMap = new HashMap<>();
+        validStates = new HashSet<>(Arrays.asList("CT", "MA", "ME", "NY", "RI", "VT"));
     }
 
     public void buildMap(String filePath) {
